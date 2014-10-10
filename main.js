1d11047e48d6e9cbdaf360bef86d0aedb4dac318
paulgarbett <paulcgarbett@gmail.com>
2014-10-09 18:16:44 -0700

    attempt to build TTT using Angular

---
+++ main.js
@@ -0,0 +1,8 @@
+	var myTicTacToe = angular.module ('myTicTacToe',[])
+
+	//creating rows in tic tac toe//
+
+	myTicTacToe.controller('myController', function($scope){
+		$scope.rows = ["rowOne"];
+	
+	});
