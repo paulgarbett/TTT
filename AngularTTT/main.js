695a2d571f0fa29850b0f21f7798021895b488f3
paulgarbett <paulcgarbett@gmail.com>
2014-10-16 14:26:59 -0700

    Got colors to change when clicked.

--- AngularTTT/main.js
+++ AngularTTT/main.js
@@ -2,27 +2,56 @@ var myTicTacToeApp = angular.module('myTicTacToeApp', []);
 
 myTicTacToeApp.controller('MyTicTacToeApp', function ($scope) {
 
+ 	$scope.testString = "Is this hooked up correctly????";
  	//test button
 
 	$scope.test = function() {
 		console.log('This is working.');
 	};
 
+	//change cell color
+
+	$scope.change = function() {
+
+	};
 
 	//creating the cells
 
-	$scope.cells = [
-	{status: "c01"},
-	{status: "c02"},
-	{status: "c03"},
-	{status: "c04"},
-	{status: "c05"},
-	{status: "c06"},
-	{status: "c07"},
-	{status: "c08"},
-	{status: "c09"},
-	];
-	
+	$scope.cellList = [
+	{status: "c01", move:""},
+	{status: "c02", move:""},
+	{status: "c03", move:""},
+	{status: "c04", move:""},
+	{status: "c05", move:""},
+	{status: "c06", move:""},
+	{status: "c07", move:""},
+	{status: "c08", move:""},
+	{status: "c09", move:""}
+	] ;
+
+	$scope.movecounter = 0 ;
+
+	$scope.select = function() {
+		console.log('Change Color.') ;
+	};
+
+	$scope.playerPicks = function(thisCell) {
+		$scope.movecounter = $scope.movecounter + 1 ;
+		console.log('Change Color.') ;
+		if (($scope.movecounter % 2) == 1) {
+			thisCell.move = "X" ;
+			
+		}
+		else {
+			thisCell.move = "O";
+			
+		}
+		console.log("Cells is now: " + thisCell.move) ;
+
+	};
+
+
+
 
 
 
